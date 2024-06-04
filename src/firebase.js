e01c368fbcb67f3c4992926b7b7ea7e38df83b1b
aishanirfan <aishanirfan6@gmail.com>
2024-06-04 21:01:37 +0530

    dowongraded react to version 17 as firebase not compatible with mde

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAuzvKKdPww6hsIUA4RBcccGi0GRRibLwY",
+  authDomain: "takenotes-552a4.firebaseapp.com",
+  projectId: "takenotes-552a4",
+  storageBucket: "takenotes-552a4.appspot.com",
+  messagingSenderId: "550552816131",
+  appId: "1:550552816131:web:74a3adae79ce8c36221c36"
+};
+
+const app = initializeApp(firebaseConfig);
